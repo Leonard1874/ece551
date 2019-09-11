57e8739c2fc8dfe6e4a6cc4811ecc3e2f81c9218
ww165 <ww165@duke.edu>
2019-09-11 19:30:45 -0400

    1st

---
+++ 037_array_subseq/maxSeq.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t maxSeq(int * array, size_t n) {
+  if (n == 0) {
+    return 0;
+  }
+  if (n == 1) {
+    return 1;
+  }
+  size_t l;
+  size_t max_l;
+  size_t left = 0;
+  size_t right = 0;
+  l = left - right;
+  while (right + 1 <= n - 1) {
+    if (array[right + 1] > array[right]) {
+      right += 1;
+      l = right - left + 1;
+    }
+    else {
+      right++;
+      left = right;
+      l = 1;
+    }
+    if (max_l < l) {
+      max_l = l;
+    }
+  }
+  return max_l;
+}

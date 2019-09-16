e625585dd36fbfee454d6878c0379ce1c2959830
ww165 <ww165@duke.edu>
2019-09-16 13:22:38 -0400

    1st

--- 041_reverse_str/reverse.c
+++ 041_reverse_str/reverse.c
@@ -4,6 +4,26 @@
 
 void reverse(char * str) {
   //WRITE ME!
+  int l = 0;
+  for (int index = 0; str[index] != '\0'; index++) {
+    l += 1;
+  }
+  if (l == 0 || l == 1) {
+    return;
+  }
+  else {
+    char temp = 'x';
+    int i = 0;
+    int j = l - 1;
+    while (i <= j) {
+      temp = str[i];
+      str[i] = str[j];
+      str[j] = temp;
+      i++;
+      j--;
+    }
+    return;
+  }
 }
 
 int main(void) {
@@ -13,7 +33,8 @@ int main(void) {
   char str3[] = "Captain's log, Stardate 42523.7";
   char str4[] = "Hello, my name is Inigo Montoya.";
   char str5[] = "You can be my wingman anyday!";
-  char str6[] = "Executor Selendis! Unleash the full power of your forces! There may be no tomorrow!";
+  char str6[] = "Executor Selendis! Unleash the full power of your forces! There may be "
+                "no tomorrow!";
   char * array[] = {str0, str1, str2, str3, str4, str5, str6};
   for (int i = 0; i < 7; i++) {
     reverse(array[i]);

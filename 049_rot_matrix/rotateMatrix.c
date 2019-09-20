906198a593ee4581974b3b6489db1725faa776d6
ww165 <ww165@duke.edu>
2019-09-20 17:09:17 -0400

    3rd-049-temp

--- 049_rot_matrix/rotateMatrix.c
+++ 049_rot_matrix/rotateMatrix.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-int readlines(char ** l, size_t size, size_t lsize, FILE * f) {
+int readlines(int ** l, size_t size, size_t lsize, FILE * f) {
   size_t i = 0;
   while (i < size) {
     /*
@@ -20,8 +20,11 @@ int readlines(char ** l, size_t size, size_t lsize, FILE * f) {
       }
       i++;
     }*/
+    /*
     fgets(l[i], lsize, f);
     if (strchr(l[i], '\n') == NULL) {
+      int t = 0;
+      if ()
       fprintf(stderr, "line is too long!");
       return 0;
     }
@@ -33,18 +36,20 @@ int readlines(char ** l, size_t size, size_t lsize, FILE * f) {
     }
     i++;
   }
+    */
 
-  char c = '1';
+    int c = 1;
 
-  if ((c = fgetc(f)) != EOF) {
-    fprintf(stderr, "too many lines!");
-    return 0;
+    if ((c = fgetc(f)) != EOF) {
+      fprintf(stderr, "too many lines!");
+      return 0;
+    }
+
+    return 1;
   }
-
-  return 1;
 }
 
-void printlines(char ** l, size_t lsize, size_t csize) {
+void printlines(int ** l, size_t lsize, size_t csize) {
   for (size_t i = 0; i < csize; i++) {
     for (size_t j = 0; j < lsize - 2; j++) {
       fprintf(stdout, "%c", l[csize - 1 - j][i]);
@@ -71,8 +76,8 @@ int main(int argc, char ** argv) {
 
   size_t lsize = 12;
   size_t csize = 10;
-  char lines[csize][lsize];
-  char * plines[] = {
+  int lines[csize][lsize];
+  int * plines[] = {
       lines[0],
       lines[1],
       lines[2],

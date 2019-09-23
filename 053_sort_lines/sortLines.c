544159130cec1d72779917c5d3999b4325c92719
ww165 <ww165@duke.edu>
2019-09-23 12:06:12 -0400

    1st-052

--- 053_sort_lines/sortLines.c
+++ 053_sort_lines/sortLines.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 //This function is used to figure out the ordering
 //of the strings in qsort.  You do not need
 //to modify it.
@@ -16,9 +15,62 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
+int rsp(FILE * f) {
+  char ** lines = NULL;
+  char * temp = NULL;
+  size_t sz;
+  size_t i = 0;
+  while (getline(&temp, &sz, f) >= 0) {
+    lines = realloc(lines, (i + 1) * sizeof(*lines));
+    lines[i] = temp;
+    temp = NULL;
+    i += 1;
+  }
+
+  free(temp);
+
+  sortData(lines, i);
+
+  for (size_t j = 0; j < i; j++) {
+    printf("%s", lines[j]);
+    free(lines[j]);
+  }
+
+  free(lines);
+  return 0;
+}
+
 int main(int argc, char ** argv) {
-  
   //WRITE YOUR CODE HERE!
-  
+
+  if (argc < 1) {
+    fprintf(stderr, "invalid argument number!");
+    return EXIT_FAILURE;
+  }
+
+  if (argc == 1) {
+    if (rsp(stdin) != 0) {
+      return EXIT_FAILURE;
+    }
+  }
+
+  if (argc > 1) {
+    int i = 1;
+    while (argv[i] != NULL) {
+      FILE * f = fopen(argv[i], "r");
+      if (f == NULL) {
+        fprintf(stderr, "cannot open file!");
+        return EXIT_FAILURE;
+      }
+      if (rsp(f) != 0) {
+        return EXIT_FAILURE;
+      }
+      if (fclose(f) != 0) {
+        fprintf(stderr, "cannot close file!");
+        return EXIT_FAILURE;
+      }
+      i += 1;
+    }
+  }
   return EXIT_SUCCESS;
 }

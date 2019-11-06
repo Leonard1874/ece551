ee72e3acb92773b0f5605cde78356e777b49a4f0
ww165 <ww165@duke.edu>
2019-11-05 23:52:50 -0500

    8th-097

--- 097_bst_map/bstmap.h
+++ 097_bst_map/bstmap.h
@@ -7,13 +7,20 @@ template<typename K, typename V>
 class BstMap : public Map<K, V> {
   class Node {
    public:
+    Node * parent;
     Node * left;
     Node * right;
     K key;
     V value;
     Node() : left(NULL), right(NULL), key(NULL), value(NULL) {}
-    Node(const K & k, const V & v) : left(NULL), right(NULL), key(k), value(v) {}
-    Node(const K & k, const V & v, Node * l, Node * r) :
+    Node(const K & k, const V & v) :
+        parent(NULL),
+        left(NULL),
+        right(NULL),
+        key(k),
+        value(v) {}
+    Node(const K & k, const V & v, Node * p, Node * l, Node * r) :
+        parent(p),
         left(l),
         right(r),
         key(k),
@@ -43,9 +50,9 @@ class BstMap : public Map<K, V> {
   }
 
  private:
-  Node * add_helper(const K & key, const V & value, Node * cur) {
+  Node * add_helper(const K & key, const V & value, Node * cur, Node * parent) {
     if (cur == NULL) {
-      cur = new Node(key, value, NULL, NULL);
+      cur = new Node(key, value, parent, NULL, NULL);
       return cur;
     }
     else {
@@ -54,10 +61,10 @@ class BstMap : public Map<K, V> {
       }
       else {
         if (cur->key > key) {
-          cur->left = add_helper(key, value, cur->left);
+          cur->left = add_helper(key, value, cur->left, cur);
         }
         else {
-          cur->right = add_helper(key, value, cur->right);
+          cur->right = add_helper(key, value, cur->right, cur);
         }
       }
       return cur;
@@ -66,122 +73,57 @@ class BstMap : public Map<K, V> {
 
  public:
   virtual void add(const K & key, const V & value) {
-    root = add_helper(key, value, root);
-  }
-
-  Node * getMin(Node * cur) {
-    if (cur->left == NULL)
-      return cur;
-
-    return getMin(cur->left);
-  }
-
-  Node * getMax(Node * cur) {
-    if (cur->right == NULL)
-      return cur;
-
-    return getMax(cur->right);
-  }
-
-  Node * removeMin(Node * cur) {
-    if (cur->left == NULL) {
-      Node * newhead = cur->right;
-      delete cur;
-      return newhead;
-    }
-
-    cur->left = removeMin(cur->left);
-    return cur;
-  }
-
-  Node * removeMax(Node * cur) {
-    if (cur->right == NULL) {
-      Node * newhead = cur->left;
-      delete cur;
-      return newhead;
-    }
-
-    cur->right = removeMax(cur->right);
-    return cur;
+    root = add_helper(key, value, root, NULL);
   }
 
  private:
-  Node * remove_helper(const K & key, Node * cur) {
-    if (cur == NULL) {
-      return cur;
-    }
-    if (cur->key == key) {
-      if (cur->left == NULL && cur->right == NULL) {
-        delete cur;
-        return NULL;
+  void remove_helper(Node * R) {
+    if (R->left == NULL && R->right == NULL) {
+      if (R->parent->left == R) {
+        R->parent->left = NULL;
+        delete R;
+        return;
       }
       else {
-        if (cur->left == NULL) {
-          Node * newhead = cur->right;
-          delete cur;
-          return newhead;
-        }
-        else if (cur->right == NULL) {
-          Node * newhead = cur->left;
-          delete cur;
-          return newhead;
-        }
-        else {
-          Node * getmin = getMin(cur->right);
-          Node * replace =
-              new Node(getmin->key, getmin->value, getmin->left, getmin->right);
-          replace->right = removeMin(cur->right);
-          replace->left = cur->left;
-          delete cur;
-          return replace;
-        }
+        R->parent->right = NULL;
+        delete R;
+        return;
       }
     }
     else {
-      if (cur->key > key) {
-        cur->left = remove_helper(key, cur->left);
-        return cur;
+      if (R->left == NULL) {
+        if (R->parent->left == R) {
+          R->parent->left = R->right;
+          R->right->parent = R->parent;
+          delete R;
+          return;
+        }
+        else {
+          R->parent->right = R->right;
+          R->right->parent = R->parent;
+          delete R;
+          return;
+        }
       }
-      else {
-        cur->right = remove_helper(key, cur->right);
-        return cur;
+      if (R->right == NULL) {
+        if (R->parent->left == R) {
+          R->parent->left = R->left;
+          R->left->parent = R->parent;
+          delete R;
+          return;
+        }
+        else {
+          R->parent->right = R->left;
+          R->left->parent = R->parent;
+          delete R;
+          return;
+        }
       }
     }
   }
 
  public:
-  virtual void remove(const K & key) { root = remove_helper(key, root); }
-
- private:
-  void printbst(Node * cur) {
-    if (cur != NULL) {
-      printbst(cur->left);
-      std::cout << "(" << cur->key << ", " << cur->value << ")" << std::endl;
-      printbst(cur->right);
-    }
-  }
-
- public:
-  void printbst() {
-    Node * cur = root;
-    printbst(cur);
-  }
-
- private:
-  void destroy(Node * current) {
-    if (current != NULL) {
-      destroy(current->left);
-      destroy(current->right);
-      delete current;
-    }
-  }
-
- public:
-  BstMap() : root(NULL) {}
-  virtual ~BstMap() { destroy(root); }
-};
-/*
-    Node * cur = root;
+  Node * find(Node * cur, K key) {
     while (cur != NULL) {
       if (cur->key == key) {
         break;
@@ -195,6 +137,12 @@ class BstMap : public Map<K, V> {
         }
       }
     }
+    return cur;
+  }
+
+  virtual void remove(const K & key) {
+    Node * cur = root;
+    cur = find(root, key);
     if (cur == NULL) {
       std::cerr << "not such node" << std::endl;
       return;
@@ -212,6 +160,7 @@ class BstMap : public Map<K, V> {
         cur->value = toRemove->value;
         toRemove->key = tmp1;
         toRemove->value = tmp2;
+        assert(toRemove->left == NULL || toRemove->right == NULL);
         remove_helper(toRemove);
       }
       else {
@@ -241,46 +190,36 @@ class BstMap : public Map<K, V> {
         }
       }
     }
-    */
-/*
-    if (R->left == NULL && R->right == NULL) {
-      if (R->parent->left == R) {
-        R->parent->left = NULL;
-        delete R;
-        return;
-      }
-      if (R->parent->right == R) {
-        R->parent->right = NULL;
-        delete R;
-        return;
-      }
+  }
+
+ private:
+  void printbst(Node * cur) {
+    if (cur != NULL) {
+      printbst(cur->left);
+      std::cout << "(" << cur->key << ", " << cur->value << ")" << std::endl;
+      printbst(cur->right);
     }
-    if (R->left == NULL) {
-      if (R->parent->left == R) {
-        R->parent->left = R->right;
-        R->right->parent = R->parent;
-        delete R;
-        return;
-      }
-      if (R->parent->right == R) {
-        R->parent->right = R->right;
-        R->right->parent = R->parent;
-        delete R;
-        return;
-      }
+  }
+
+ public:
+  void printbst() {
+    Node * cur = root;
+    printbst(cur);
+  }
+
+ private:
+  void destroy(Node * current) {
+    if (current != NULL) {
+      destroy(current->left);
+      destroy(current->right);
+      delete current;
     }
-    if (R->right == NULL) {
-      if (R->parent->left == R) {
-        R->parent->left = R->left;
-        R->left->parent = R->parent;
-        delete R;
-        return;
-      }
-      if (R->parent->right == R) {
-        R->parent->right = R->left;
-        R->left->parent = R->parent;
-        delete R;
-        return;
-      }
-    }
-    */
+  }
+
+ public:
+  BstMap() : root(NULL) {}
+  virtual ~BstMap() {
+    Node * current = root;
+    destroy(current);
+  }
+};

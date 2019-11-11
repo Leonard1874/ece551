d9b8b7ff6fafd2df9a51f5cde12d80808cc59905
ww165 <ww165@duke.edu>
2019-11-11 11:37:08 -0500

    1st-102

--- 102_huff_tree/buildTree.cpp
+++ 102_huff_tree/buildTree.cpp
@@ -1,5 +1,21 @@
 #include "node.h"
 
-Node *  buildTree(uint64_t * counts) {
+Node * buildTree(uint64_t * counts) {
   //WRITE ME!
-}  
+  priority_queue_t pq;
+  for (unsigned i = 0; i < 257; i++) {
+    if (counts[i] > 0) {
+      pq.push(new Node(i, counts[i]));
+    }
+  }
+  while (pq.size() > 1) {
+    if (pq.size() >= 2) {
+      Node * a = pq.top();
+      pq.pop();
+      Node * b = pq.top();
+      pq.pop();
+      pq.push(new Node(a, b));
+    }
+  }
+  return pq.top();
+}

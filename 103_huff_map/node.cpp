d92d94a4924a47748eb9ad78972744ae2736d982
ww165 <ww165@duke.edu>
2019-11-11 12:07:04 -0500

    1st-103

--- 103_huff_map/node.cpp
+++ 103_huff_map/node.cpp
@@ -1,5 +1,21 @@
 #include "node.h"
-void Node::buildMap(BitString b, std::map<unsigned,BitString> & theMap) {
-  //WRITE ME!
+void buildMap_helper(Node * node, BitString b, std::map<unsigned, BitString> & theMap) {
+  if (node->sym != NO_SYM) {
+    theMap[node->sym] = b;
+    return;
+  }
+  else {
+    if (node->left != NULL) {
+      buildMap_helper(node->left, b.plusZero(), theMap);
+    }
+    if (node->right != NULL) {
+      buildMap_helper(node->right, b.plusOne(), theMap);
+    }
+  }
 }
 
+void Node::buildMap(BitString b, std::map<unsigned, BitString> & theMap) {
+  //WRITE ME!
+  Node * root = this;
+  buildMap_helper(root, b, theMap);
+}

c5074c69d88abe70d93a6bae244313ca1fba3464
ww165 <ww165@duke.edu>
2019-11-11 13:06:40 -0500

    1st-104

--- 102_huff_tree/testTree.cpp
+++ 102_huff_tree/testTree.cpp
@@ -1,6 +1,7 @@
+#include <iostream>
+
 #include "node.h"
 #include "readFreq.h"
-#include <iostream>
 
 void indentBy(int n) {
   for (int i = 0; i < n; i++) {
@@ -8,13 +9,13 @@ void indentBy(int n) {
   }
 }
 int printTree(int indent, Node * t) {
-  assert (t != NULL);
+  assert(t != NULL);
   if (t->sym == NO_SYM) {
     std::cout << "(";
     int rpos = printTree(indent + 1, t->left);
     if (rpos == 0) {
       std::cout << std::endl;
-      rpos = indent +1;
+      rpos = indent + 1;
       indentBy(rpos);
     }
     printTree(rpos, t->right);
@@ -26,10 +27,10 @@ int printTree(int indent, Node * t) {
     printSym(std::cout, t->sym);
     std::cout << ": ";
     std::streamsize w = std::cout.width(6);
-    std::cout << t->freq ;
+    std::cout << t->freq;
     std::cout.width(w);
     std::cout << ">";
-    return indent + 1 + 3+ 2 + 6 + 1;
+    return indent + 1 + 3 + 2 + 6 + 1;
   }
 }
 
@@ -38,9 +39,10 @@ int main(int argc, char ** argv) {
     std::cerr << "Usage: " << argv[0] << " (input file)" << std::endl;
     return EXIT_FAILURE;
   }
+
   uint64_t * counts = readFrequencies(argv[1]);
   assert(counts != NULL);
-  Node * tree = buildTree (counts);
+  Node * tree = buildTree(counts);
   delete[] counts;
   printTree(0, tree);
   std::cout << std::endl;
